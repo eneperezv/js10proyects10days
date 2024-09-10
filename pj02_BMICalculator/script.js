bdf3b8b4892d059885939878cdf9843f92731f49
eneperezv <eneperezv@gmail.com>
2024-09-09 21:07:35 -0500

    Se incluye calculo del bmi segun los datos recibidos en el html.
     Changes to be committed:
    	modified:   pj02_BMICalculator/script.js

--- pj02_BMICalculator/script.js
+++ pj02_BMICalculator/script.js
@@ -4,4 +4,15 @@ document.getElementById("bmiForm").addEventListener('submit',function(e){
     let heightFeet   = parseInt(document.getElementById('height-feet').value);
     let heightInches = parseInt(document.getElementById('height-inches').value);
     let weight       = parseFloat(document.getElementById('weight').value);
+    if(gender && age && heightFeet && heightInches && weight){
+        var heightInMetters = ((height * 12) + heightInches) * 0.0254;
+        var bmi = weight / (heightInMetters * heightInMetters);
+        var resultElement = document.getElementById('result');
+
+        var category = '';
+
+        if(bmi < 18.5){
+            category = 'Under Weight';
+        }
+    }
 });
