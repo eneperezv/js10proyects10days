0177eed1dc346fbb032d910163bd7ba1715a4939
eneperezv <eneperezv@gmail.com>
2024-09-09 21:03:20 -0500

    Se incluye lectura de campos desde el html.
     Changes to be committed:
    	modified:   pj02_BMICalculator/script.js

--- pj02_BMICalculator/script.js
+++ pj02_BMICalculator/script.js
@@ -1,3 +1,7 @@
 document.getElementById("bmiForm").addEventListener('submit',function(e){
-    
+    let gender       = document.getElementById('gender').value;
+    let age          = parseInt(document.getElementById('age').value);
+    let heightFeet   = parseInt(document.getElementById('height-feet').value);
+    let heightInches = parseInt(document.getElementById('height-inches').value);
+    let weight       = parseFloat(document.getElementById('weight').value);
 });
