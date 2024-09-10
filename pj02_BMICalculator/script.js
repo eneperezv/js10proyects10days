7bda60ddce6c53be971f26213caa3033936ef890
eneperezv <eneperezv@gmail.com>
2024-09-09 21:19:27 -0500

    Se finaliza desarrollo de calculo y visualizacion de informacion de resultado.
     Changes to be committed:
    	modified:   pj02_BMICalculator/script.js

--- pj02_BMICalculator/script.js
+++ pj02_BMICalculator/script.js
@@ -1,11 +1,12 @@
-document.getElementById("bmiForm").addEventListener('submit',function(e){
-    let gender       = document.getElementById('gender').value;
-    let age          = parseInt(document.getElementById('age').value);
-    let heightFeet   = parseInt(document.getElementById('height-feet').value);
-    let heightInches = parseInt(document.getElementById('height-inches').value);
-    let weight       = parseFloat(document.getElementById('weight').value);
+document.getElementById("bmiForm").addEventListener("submit",function(e){
+    e.preventDefault();
+    var gender       = document.getElementById('gender').value;
+    var age          = parseInt(document.getElementById('age').value);
+    var heightFeet   = parseInt(document.getElementById('height-feet').value);
+    var heightInches = parseInt(document.getElementById('height-inches').value);
+    var weight       = parseFloat(document.getElementById('weight').value);
     if(gender && age && heightFeet && heightInches && weight){
-        var heightInMetters = ((height * 12) + heightInches) * 0.0254;
+        var heightInMetters = ((heightFeet * 12) + heightInches) * 0.0254;
         var bmi = weight / (heightInMetters * heightInMetters);
         var resultElement = document.getElementById('result');
 
@@ -20,5 +21,10 @@ document.getElementById("bmiForm").addEventListener('submit',function(e){
         }else{
             category = 'Obese';
         }
+
+        let resultMessage = 'Your BMI:' + bmi.toFixed(2) + '<br>';
+        resultMessage += 'Category:' + category;
+
+        resultElement.innerHTML = resultMessage;
     }
 });
